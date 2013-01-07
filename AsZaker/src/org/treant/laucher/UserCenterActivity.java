dda649441bb96bd27867f2efe05c0302975fc6ef
Draenei <faye2046@126.com>
2013-01-07 17:50:48 +0800

    原始界面风格的

---
+++ AsZaker/src/org/treant/laucher/UserCenterActivity.java
@@ -0,0 +1,5 @@
+package org.treant.laucher;
+
+public class UserCenterActivity {
+
+}
---
+++ AsZaker/src/org/treant/laucher/util/IntentData.java
@@ -0,0 +1,9 @@
+package org.treant.laucher.util;
+
+
+public class IntentData{
+	
+	public static String _addItem;
+
+	public static int PicFrom = 0;
+}
